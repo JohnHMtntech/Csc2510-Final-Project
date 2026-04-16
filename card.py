154c9d3be1a73c1556b1e2b9191b528c11c5fbf8
John Miller <jhmiller43@tntech.edu>
2026-04-16 13:12:16 -0500

    Added error handling for Card construtor

--- card.py
+++ card.py
@@ -1,10 +1,24 @@
 class Card:
+    _suits = ["spades", "clubs", "hearts", "diamonds"]
+    _ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"]
     suits = ["S", "C", "H", "D"]
     ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-    def __init__(self, rank, suit="S"):
+    def __new__(cls, rank, suit="S"):
+        try:
+            Card.ranks.index(rank)
+        except:
+            return None
+        try:
+            Card.suits.index(suit)
+        except:
+            return None
+        return super(Card, cls).__new__(cls)
+    
+    def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
+        self.texture = Card._ranks[Card.ranks.index(self.rank)]+"_of_"+Card._suits[Card.suits.index(self.suit)]+".png"
 
     def __str__(self):
-        return "Rank: " + self.rank + " Suit: " + self.suit
+        return self.texture
--- game_manager.py
+++ game_manager.py
@@ -2,12 +2,19 @@ from card import Card
 import random
 
 class Game:
-    def __init__(self, starting_chips=250):
+    def __init__(self, player_name, starting_chips=250):
+        self.player_name = player_name
         self.chips = starting_chips
         self.deck = []
         for suit in Card.suits:
             for rank in Card.ranks:
                 self.deck.append(Card(rank, suit))
+
+    def __str__(self):
+        value = self.player_name+": "+str(self.chips)+" chips"
+        for card in self.deck:
+            value = value+"\n  "+str(card)
+        return value
 
     def shuffle_deck(self):
         old_deck = self.deck.copy()
@@ -21,7 +28,15 @@ class Game:
             while copyed_cards[random_card]:
                 random_card = random.randint(0,len(old_deck)-1)
             self.deck.append(old_deck[random_card])
-        
 
-#if __name__ == "__main__":
-#    test_game = Game()
+    def draw_card(self):
+        return self.deck.pop(0)
+        
+'''
+if __name__ == "__main__":
+    test_game = Game("Jeff")
+    test_game.shuffle_deck()
+    print(test_game)
+
+    print(Card("jeff"))
+'''
